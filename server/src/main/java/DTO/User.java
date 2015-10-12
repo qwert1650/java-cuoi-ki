93a16bbc60988fb1cabb84872bfc973b930e64b9
qwert1650 <hongoctuan.it@gmail.com>
2015-10-12 18:23:22 +0700

    noi dung
    cap nhat

---
+++ server/src/main/java/DTO/User.java
@@ -0,0 +1,38 @@
+package DTO;
+
+import java.util.List;
+
+public class User {
+
+    private String userName;
+    private String password;
+    private List<String> accessibleSheets;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<String> getAccessibleSheets() {
+        return accessibleSheets;
+    }
+
+    public void setAccessibleSheets(List<String> accessibleSheets) {
+        this.accessibleSheets = accessibleSheets;
+    }
+
+    public User(){
+
+    }
+}
